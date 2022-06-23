5818fe3d532564f751601eeecd14fc2b700a111b
bumbleIV <linganda@msu.edu>
2022-06-23 12:13:59 -0700

    progress in algorithm

--- Brownian Motion Simulation/main.py
+++ Brownian Motion Simulation/main.py
@@ -17,9 +17,9 @@
 
 # Algorithm:
 # 1. Initialize M = {}
-# 2. Attribute each node in V input amount of blue and red balls
-# 3. Randomize then fix the order of nodes to in V and record new order in V' = {}
-# 4. Select V'[i] as current_node
+# 2. Attribute each node in V a node_iD, blue_amt and red_amt balls
+# 3. Copy V into V_2 then randomize (and fix) the order of nodes in V_2
+# 4. Select V_2[i] as current_node
 # 5. Record the current_node in M for each ball attributed to current_node
 # 6. Select a neighbor_node of current_node
 # 7. Record the neighbor_node in M for each ball attributed to neighbor_node
@@ -27,7 +27,7 @@
 #    a. If both balls sampled are the same color, the no transaction occurs
 #    b. If both balls sampled are different colors, the red ball is removed from its owner node and given to the opposing node
 # 9. Repeat steps 6-9 until all neighbor_node of current_node are visited
-# 10. Select next current_node in V'
+# 10. Select next current_node in V_2
 # 11. Repeat steps 5-10 until n iterations are completed
 # 12. Return M
 
@@ -41,27 +41,57 @@ import numpy as np
 import random
 
 
-def RedBallBlueBall(G, n: int, blue: int, red: int) -> dict:
+def RedBallBlueBall(G, n: int, blue_amt: int, red_amt: int) -> dict:
     # 1. Initialize M = {}
     M = {}
 
-    # 2. Attribute each node in V input amount of blue and red balls
-    for node in G.nodes():
+    # 2. Attribute each node in V blue_amt and red_amt balls
+    for i in G:
+        # create a list of blue balls with blue_amt amount of balls for each node
+        blue_balls = ["{}_{}".format(
+            "blue",
+            j + (i * blue_amt)) for j in range(blue_amt)]
+
+        # create a list of red balls with red_amt amount of balls for each node
+        red_balls = ["{}_{}".format(
+            "red",
+            j + (i * red_amt)) for j in range(red_amt)]
+
+        # attribute blue_balls and red_balls to node
+        G.nodes[i]['Blue Balls'] = blue_balls
+        G.nodes[i]['Red Balls'] = red_balls
+
+    # 3. Copy V into V_2 then randomize (and fix) the order of nodes in V_2
+    V_2 = list(G.nodes)  # V = list(G.nodes)
+    random.shuffle(V_2)
+
+    # 4. Select V_2[i] as current_node
+    for i in range(n):
         pass
 
     return M
 
 
 def main():
+    # Input:
+
+    # Network Model: G = (V, E)
     # generate a random graph with a random number of nodes and edges (between 5 and 30 for now)
     G = nx.gnm_random_graph(n=random.randint(5, 30), m=random.randint(5, 30))
+
+    # Output:
+    M = RedBallBlueBall(G, n=10, blue_amt=5, red_amt=5)
 
     # draw the graph
     # nx.draw(G, with_labels=False, node_size=10)
     # plt.show()
 
-    M = RedBallBlueBall(G, n=10, blue=5, red=5)
+    # Insert Data Analysis Code of M here
+    pass
 
 
 if __name__ == "__main__":
     main()
+
+
+# Could I possibly create a Ball object that has an ID, a color, and a list of nodes it has traveled to?
